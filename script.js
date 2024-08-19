cdfbb88f455e91f361851dd066bb06cac1c46b96
Naveen <naveenv7010@gmail.com>
2024-08-19 08:46:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function updateCountdown() {
+    const now = new Date();
+    const newYear = new Date(now.getFullYear() + 1, 0, 1);
+    
+    const timeDiff = newYear - now;
+    console.log(timeDiff);
+    
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = days.toString().padStart(2, '0');
+    document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+    document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+}
+setInterval(updateCountdown, 1000);
+updateCountdown();
